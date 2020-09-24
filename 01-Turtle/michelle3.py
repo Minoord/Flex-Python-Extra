b59d747346c0cd92ba6ab23dd850abec04da914c
Michelle <Minoord18@outlook.com>
2020-09-24 09:52:42 +0200

    Verschillende rondjes

---
+++ 01-Turtle/michelle3.py
@@ -0,0 +1,18 @@
+import turtle             
+colors = [ "red","purple","blue","green","orange","yellow"]
+turtle.speed(0)
+my_pen = turtle.Pen()
+turtle.bgcolor("black")
+for x in range(360):
+   my_pen.pencolor(colors[x % 6])
+   my_pen.width(x/100 + 1)
+   my_pen.circle(x)
+   my_pen.right(90)
+   my_pen.circle(x)
+   my_pen.right(90)
+   my_pen.circle(x)
+   my_pen.right(90)
+   my_pen.circle(x)
+   my_pen.right(90)
+   
+turtle.done()
